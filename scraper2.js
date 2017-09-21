f7244509e95449e55942e435064caa5da786a7d4
tigranhovsepyans <tigran.hovsepyan.s@tumo.org>
2017-09-21 20:36:03 +0400

    scrap
    scrap

---
+++ scraper2.js
@@ -0,0 +1,29 @@
+var scrapeIt = require("scrape-it");
+var jsonfile = require('jsonfile');
+var file = 'data/data2.json';
+
+
+scrapeIt("http://www.4icu.org/fr/", {
+	franchuniversity: {
+
+		listItem: ".table > tbody > tr",
+		data:{
+
+			rank: "td > kbd",
+
+			title: "td > a",
+
+			location: 'td:nth-child(3)',
+
+		}
+	}
+
+
+    
+}).then(page => {
+    console.log(page);
+    jsonfile.writeFile(file, page, {section: 2 }, function (err) {
+        console.error(err);
+
+    })
+});
